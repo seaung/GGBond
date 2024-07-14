88c3c6757ea17fa4a3e491a4edde1b569307c5a8
seaung <seaung.c0m7de@protonmail.com>
2024-07-14 23:21:06 +0800

    feat(添加配置)

--- backend/app/configs/config.py
+++ backend/app/configs/config.py
@@ -7,3 +7,22 @@ CURRENT_DIR = os.path.abspath(__file__)
 # 项目根目录
 ROOT_PATH = os.path.dirname(os.path.dirname(CURRENT_DIR))
 
+
+class BaseConfig(object):
+    DEBUG = False
+
+
+class Development(BaseConfig):
+    pass
+
+
+class Productions(BaseConfig):
+    pass
+
+
+
+configs = {
+        'dev': Development(),
+        'prod': Productions()
+}
+
---
+++ backend/app/models/tasks.py
@@ -0,0 +1,2 @@
+class TaskModel():
+    pass
