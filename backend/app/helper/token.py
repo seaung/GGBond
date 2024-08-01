c78e585c8ebedb0de013696b24f15dad5cafc7ae
seaung <seaung.c0m7de@protonmail.com>
2024-08-01 19:33:21 +0800

    feat: 添加依赖

--- backend/app/helper/token.py
+++ backend/app/helper/token.py
@@ -1,11 +1,27 @@
+import jwt
+
+from typing import Any, Optional
 from datetime import datetime, timedelta
-from jose import JWTError, jwt
-from passlib.context import CryptContext
+from starlette.requests import Request
 
-from typing import Union
+from app.configs.config import settings
+from app.types.user import Token
+from app.models.user import User
 
 
-def create_token(data: dict, expires_delta: Union[timedelta, None] = None):
-    pass
+def create_token(data: dict, expires_delta: Optional[timedelta] = None) -> Any:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({'expire': expire})
+
+    encode_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.TOKEN_ALGORITHM)
+    return encode_jwt
+
+
+def generate_token(request: Request, username: str, password: str) -> Token:
+    ...
 
 
