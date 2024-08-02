9e74fa8edeb3d1499745b02a7045324108fb4f47
seaung <seaung.c0m7de@protonmail.com>
2024-08-02 23:58:12 +0800

    feat:改为ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,6 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(store).use(router).mount('#app')
